edb80f801d3116747b4df0527d773a650d1e0bb8
vishal <vishal@qux.dev>
2023-11-14 06:04:34 +0530

    Enriched settings

--- settings.py
+++ settings.py
@@ -1,4 +1,18 @@
+import os
+
 from django.conf import settings
+
+
+def default_project_setting_to_app(setting, default):
+    return getattr(settings, setting, os.getenv(setting, default))
+
+
+def default_app_setting_to_project(setting, default):
+    return os.getenv(setting, getattr(settings, setting, default))
+
 
 QUXAPP_SETTING="Qux"
 
+# Examples
+DATA_DIR = default_project_setting_to_app("DATA_DIR", "/dev/null")
+NUM_DAYS = default_app_setting_to_project("NUM_DAYS", 1000)
