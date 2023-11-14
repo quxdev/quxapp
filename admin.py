b4b176e7cec4e008b858f8649f6d82775c20805a
vishal <vishal@qux.dev>
2023-11-14 06:04:52 +0530

    Lint free code

--- admin.py
+++ admin.py
@@ -1,6 +1,5 @@
 from django.apps import apps
 from django.contrib import admin
-from django.db import models
 
 from qux.admin import QuxModelAdmin
 
@@ -10,4 +9,3 @@ app = apps.get_app_config("quxapp")
 app_models = [x for x in app.get_models("quxapp") if x not in excluded]
 for model in app_models:
     admin.site.register(model, QuxModelAdmin)
-
